f6369b4b6f2b2e46dda31e9b02f77eae3e44d7c0
Bennett Smrdel <fireknights318@gmail.com>
2024-09-11 21:17:29 -0500

    feat: Can spawn tower sprites

--- build_tower.gd
+++ build_tower.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 
-func _on_input_event(viewport, event, shape_idx):
+func _on_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == 1 and event.pressed == false:
 		get_node("/root/Game/HUD/BuildUI").visible = true
+		var tower_builder = get_node("/root/Game/HUD/BuildUI/Panel/TowerOptions")
+		var pos = to_local(get_node("CollisionShape2D").global_position)
+		tower_builder.selected_tower_location.emit(pos)
--- tower_building.gd
+++ tower_building.gd
@@ -1,20 +1,35 @@
 extends MarginContainer
 
+signal selected_tower_location(pos)
+
+var tower_position
+
 
 func _on_build_cannon_btn_pressed():
-	print("build cannon")
+	build_tower("cannon")
 	close_ui()
 
 
 func _on_build_ice_btn_pressed():
-	print("build ice")
+	build_tower("ice")
 	close_ui()
 
 
 func _on_build_fire_btn_pressed():
-	print("build fire")
+	build_tower("fire")
 	close_ui()
+
+
+func build_tower(type: String):
+	var tower_scene = load("res://{tower}_tower.tscn".format({"tower": type}))
+	var tower = tower_scene.instantiate()
+	tower.position = tower_position
+	get_node("/root/Game/Level1").add_child(tower)
 
 
 func close_ui():
 	get_node("/root/Game/HUD/BuildUI").visible = false
+
+
+func _on_selected_tower_location(pos):
+	tower_position = pos
