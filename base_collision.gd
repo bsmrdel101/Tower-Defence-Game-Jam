43957570651b4d6d408d23497f3d2a66246a24ea
Bennett Smrdel <fireknights318@gmail.com>
2024-09-14 18:19:57 -0500

    fix: Delete enemies

--- base_collision.gd
+++ base_collision.gd
@@ -2,5 +2,7 @@ extends Area2D
 
 
 func _on_area_entered(area):
-	if area.get_parent() and area.get_parent().is_in_group("enemy"):
+	var parent = area.get_parent()
+	if parent and parent.is_in_group("enemy"):
 		get_node("/root/Game").emit_signal("player_take_damage", 3)
+		parent.queue_free()
